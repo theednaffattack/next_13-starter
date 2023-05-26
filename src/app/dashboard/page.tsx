e87590e24cc2280aa565f6cf05a82069de18368c
Eddie Naff <eddienaff@gmail.com>
2023-05-26 14:30:09 -0700

    Practice error handling

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,19 @@
 import axios from "axios";
+import { wait } from "../../util/wait";
+import { AuthRequiredError } from "../lib/exceptions";
 
 interface PageProps {}
+
+const session = null;
 
 export default async function Page({}: PageProps) {
   const { data } = await axios.get(
     "https://jsonplaceholder.typicode.com/posts/1"
   );
+
+  // await wait(3000);
+  if (!session) throw new AuthRequiredError();
+
   return (
     <div>
       Dashboard page
