a3f3f2adc81d6549f4e701bec75d1e14f201f779
Eddie Naff <eddienaff@gmail.com>
2023-05-26 13:26:54 -0700

    Practice requesting data

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,15 @@
+import axios from "axios";
+
 interface PageProps {}
 
-export function Page({}: PageProps) {
-  return <div>Dashboard page</div>;
+export default async function Page({}: PageProps) {
+  const { data } = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts/1"
+  );
+  return (
+    <div>
+      Dashboard page
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }
-
-export default Page;
