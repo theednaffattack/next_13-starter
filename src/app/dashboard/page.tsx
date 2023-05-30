9cd3d9f7fb7f851f1d75f908d6d7df193fdf920f
Eddie Naff <eddienaff@gmail.com>
2023-05-30 15:20:21 -0700

    Make session true for manual viewing

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,37 @@
 import axios from "axios";
 import { wait } from "../../util/wait";
-import { AuthRequiredError } from "../lib/exceptions";
+import { AuthRequiredError } from "../../lib/exceptions";
+import { Post } from "../sitemap";
 
 interface PageProps {}
 
-const session = null;
+const session = "fake-sesh";
 
 export default async function Page({}: PageProps) {
-  const { data } = await axios.get(
-    "https://jsonplaceholder.typicode.com/posts/1"
+  const { data } = await axios.get<Post[]>(
+    "https://jsonplaceholder.typicode.com/posts"
   );
 
-  // await wait(3000);
   if (!session) throw new AuthRequiredError();
 
   return (
     <div>
       Dashboard page
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <h1>Posts</h1>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>
+            <h1>
+              <a
+                href={`http://localhost:9090/post/${post.id}?title=${post.title}&body=${post.body}`}
+              >
+                {post.title}
+              </a>
+            </h1>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
